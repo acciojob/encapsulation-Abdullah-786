3b72f89d1b8a98b7fc3c06c0c0276e351bc1dd56
MD ABDUL RAHMAN <56754013+Abdullah-786@users.noreply.github.com>
2023-07-27 01:08:41 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args){
         RWOnly obj = new RWOnly();
 
-        obj.setName("Abdul");
+        obj.setName("Abdull");
         obj.getName();
     }
 
